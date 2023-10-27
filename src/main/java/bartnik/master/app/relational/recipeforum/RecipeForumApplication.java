1a06d61dd75f25d399d8bfc1d4c02b765efbe86b
= <=>
2023-10-27 18:50:58 +0200

    Init

---
+++ src/main/java/bartnik/master/app/relational/recipeforum/RecipeForumApplication.java
@@ -0,0 +1,13 @@
+package bartnik.master.app.relational.recipeforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bartnik/master/app/relational/recipeforum/RecipeForumApplicationTests.java
@@ -0,0 +1,13 @@
+package bartnik.master.app.relational.recipeforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
