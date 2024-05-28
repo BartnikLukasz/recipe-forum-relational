2928465f7f598eaee583b7a6cb0163abd15ea349
BartnikLukasz <bartnik.luk@gmail.com>
2024-05-28 10:22:19 +0200

    Fixed recommendation engine

--- src/main/java/bartnik/master/app/relational/recipeforum/repository/CustomUserRepositoryCrud.java
+++ src/main/java/bartnik/master/app/relational/recipeforum/repository/CustomUserRepositoryCrud.java
@@ -1,20 +1,17 @@
 package bartnik.master.app.relational.recipeforum.repository;
 
 import bartnik.master.app.relational.recipeforum.model.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @Component
@@ -35,55 +32,64 @@ public class CustomUserRepositoryCrud {
         var userLikedRecipes = mongoTemplate.find(Query.query(Criteria.where("id").is(userId)), CustomUser.class)
                 .get(0).getLikedRecipes().stream().map(Recipe::getId).toList();
 
-        var topUsers = mongoTemplate.aggregate(Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("id").in(userLikedRecipes)),
-                Aggregation.group("user").count().as("RecipeCount"),
-                Aggregation.sort(Sort.Direction.DESC, "RecipeCount"),
-                Aggregation.limit(5)
-        ), Recipe.class, CustomUser.class).getMappedResults().stream().map(CustomUser::getId).toList();
+        var similarUsersWithCommonLikes = mongoTemplate.aggregate(
+                        Aggregation.newAggregation(
+                                Aggregation.match(Criteria.where("likedRecipes").in(userLikedRecipes).and("id").ne(userId)),
+                                Aggregation.unwind("likedRecipes"),
+                                Aggregation.match(Criteria.where("likedRecipes").in(userLikedRecipes)),
+                                Aggregation.group("id")
+                                        .count().as("commonLikes")
+                                        .addToSet("likedRecipes").as("commonLikedRecipes"),
+                                Aggregation.lookup(
+                                        "CustomUser", // assuming the collection name is "users"
+                                        "_id",
+                                        "_id",
+                                        "allUserLikes"
+                                ),
+                                Aggregation.unwind("allUserLikes"),
+                                Aggregation.group("_id")
+                                        .first("commonLikes").as("commonLikes")
+                                        .first("commonLikedRecipes").as("commonLikedRecipes")
+                                        .addToSet("allUserLikes.likedRecipes").as("allLikedRecipes")
+                        ), CustomUser.class, Map.class)
+                .getMappedResults().stream()
+                .collect(Collectors.toMap(
+                        map -> (UUID) map.get("_id"),
+                        map -> new AbstractMap.SimpleEntry<>(((Number) map.get("commonLikes")).intValue(), ((List<List<UUID>>) map.get("allLikedRecipes")).stream().flatMap(List::stream).toList())
+                ));
 
-        var topCategories = mongoTemplate.aggregate(Aggregation.newAggregation(
-                Aggregation.match(Criteria.where("id").in(userLikedRecipes)),
-                Aggregation.group("category").count().as("RecipeCount"),
-                Aggregation.sort(Sort.Direction.DESC, "RecipeCount"),
-                Aggregation.limit(3)
-        ), Recipe.class, Category.class).getMappedResults().stream().map(Category::getId).toList();
-
-        var similarUsers = mongoTemplate.aggregate(
+        var recommendedRecipeIdsAndCountMap = mongoTemplate.aggregate(
                 Aggregation.newAggregation(
-                        Aggregation.match(Criteria.where("likedRecipes").in(userLikedRecipes).and("id").ne(userId)),
-                        Aggregation.unwind("likedRecipes"),
-                        Aggregation.group("id").addToSet("likedRecipes").as("likedRecipes")
-                ), CustomUser.class, CustomUser.class).getMappedResults().stream().map(CustomUser::getId).toList();
-
-        var recommendedRecipeIds = mongoTemplate.aggregate(
-                Aggregation.newAggregation(
-                        Aggregation.match(Criteria.where("id").in(similarUsers)),
+                        Aggregation.match(Criteria.where("id").in(similarUsersWithCommonLikes.keySet())),
                         Aggregation.unwind("likedRecipes"),
                         Aggregation.match(Criteria.where("likedRecipes").nin(userLikedRecipes)),
                         Aggregation.group("likedRecipes").count().as("count"),
-                        Aggregation.project()
-                                .and("count").as("count")
-                                .and("likedRecipes.user").as("user")
-                                .and("likedRecipes.category").as("category"),
-                        Aggregation.project()
-                                .and("count").as("count")
-                                .and("user").as("user")
-                                .and("category").as("category")
-                                .and(ConditionalOperators.when(Criteria.where("user").in(topUsers)).then(2).otherwise(1)).as("userMultiplier"),
-                        Aggregation.project()
-                                .and("count").as("count")
-                                .and("user").as("user")
-                                .and("category").as("category")
-                                .and("userMultiplier").as("userMultiplier")
-                                .and(ConditionalOperators.when(Criteria.where("category").in(topCategories)).then(2).otherwise(1)).as("categoryMultiplier"),
-                        Aggregation.project()
-                                .and("count").multiply("userMultiplier").multiply("categoryMultiplier").as("adjustedCount"),
-                        Aggregation.sort(Sort.Direction.DESC, "adjustedCount"),
-                        Aggregation.limit(size)
-                ), CustomUser.class, Recipe.class);
+                        Aggregation.sort(Sort.Direction.DESC, "count"),
+                        Aggregation.limit(size*10)
+                ), CustomUser.class, Map.class).getMappedResults().stream()
+                .collect(Collectors.toMap(map -> (UUID) map.get("_id"), map -> ((Number) map.get("count")).intValue()));
 
-        return new HashSet<>(mongoTemplate.find(Query.query(Criteria.where("id").in(recommendedRecipeIds.getMappedResults().stream().map(Recipe::getId).collect(Collectors.toList()))), Recipe.class));
+        Map<UUID, Integer> returned = new HashMap<>();
 
+        for (Map.Entry<UUID, Integer> entry : recommendedRecipeIdsAndCountMap.entrySet()) {
+            var recipeId = entry.getKey();
+            AtomicReference<Integer> count = new AtomicReference<>(entry.getValue());
+
+            similarUsersWithCommonLikes.forEach((uuid, map) -> {
+                if (map.getValue().contains(recipeId)) {
+                    count.updateAndGet(v -> v + map.getKey());
+                }
+            });
+
+            returned.put(recipeId, count.get());
+        }
+
+        List<UUID> sortedRecipes = returned.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .map(Map.Entry::getKey)
+                .limit(size)
+                .toList();
+
+        return new HashSet<>(mongoTemplate.find(Query.query(Criteria.where("id").in(sortedRecipes)), Recipe.class));
     }
 }
